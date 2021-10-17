a8299c588355fe8b61adf9dc1165c15bc326ad7f
bubbleCHOU <1413791340@qq.com>
2021-10-18 00:22:21 +0800

    封装点赞组件

---
+++ components/like/like.js
@@ -0,0 +1,76 @@
+// components/like/like.js
+const api = require("../../utils/api");
+
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    // 资源id
+    itemId: {
+      type: Number,
+      value: 0
+    },
+    // 评论id
+    cid: {
+      type: Number,
+      value: 0
+    },
+    // 点赞数目
+    likedCount: {
+      type: Number,
+      value: 0
+    },
+    // 是否点赞
+    liked: {
+      type: Boolean,
+      value: false
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    // 组件内用于展示的点赞数目和点赞状态
+    myLikedCount: 0,
+    myLiked: false
+  },
+
+  observers: {
+    'likedCount': function (val) {
+      if (val !== undefined) {
+        this.setData({
+          myLikedCount: val
+        })
+      }
+    },
+    'liked': function (val) {
+      if (val !== undefined) {
+        this.setData({
+          myLiked: val
+        })
+      }
+    },
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    like: function () {
+      api.like({
+        id: this.properties.itemId,
+        cid: this.properties.cid,
+        t: this.properties.liked ? 0 : 1,
+        type: 0
+      }).then(res => {
+        console.log(res);
+        this.setData({
+          myLiked: !this.data.myLiked,
+          myLikedCount: this.data.myLikedCount++
+        })
+      })
+    }
+  }
+})
